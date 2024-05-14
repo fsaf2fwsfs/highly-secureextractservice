daf4f7345ea54f481069d74a0835ed96f2ff7d84
fsaf2fwsfs <js1rslezash@hotmail.com>
2024-05-14 21:47:36 +0700

    test project

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function maxProfit(prices) {
-  let minPrice = Infinity;
-  let maxProfit = 0;
-  for (const price of prices) {
-    minPrice = Math.min(minPrice, price);
-    maxProfit = Math.max(maxProfit, price - minPrice);
-  }
-  return maxProfit;
+function getMin(arr) {
+  return Math.min(...arr);
 }
